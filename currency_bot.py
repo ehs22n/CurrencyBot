725cda0eea93f255c7824a531978beb1ca4591e9
Ehsan <ehs22nn@gmail.com>
2025-01-27 10:33:20 -0800

    Update currency_bot.py

--- currency_bot.py
+++ currency_bot.py
@@ -10,7 +10,8 @@ bot = telebot.TeleBot(token)
 conn = sqlite3.connect('currency.db' , check_same_thread=False)
 cursor = conn.cursor()
 cursor.execute('''CREATE TABLE IF NOT EXISTS currency (
-               chatid INTEGER ,
+               id INTEGER PRIMARY KEY AUTOINCREMENT,
+               chatid INTEGER NOT NULL UNIQUE ,
                username TEXT ,
                firstname TEXT
                );
@@ -54,7 +55,7 @@ combind.add(currency_btn1, currency_btn, glass_button)
 @bot.message_handler(commands=['start'])
 def start(msg):
     try:
-        username = message.from_user.username if message.from_user.username else 'unknown'
+        username = msg.from_user.username if msg.from_user.username else "unknown"
         conn = sqlite3.connect('currency.db' , check_same_thread=False)
         cursor = conn.cursor()
         cursor.execute('''INSERT INTO currency (chatid,username,firstname)
